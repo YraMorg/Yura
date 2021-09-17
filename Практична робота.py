18087ebc06bb5bbeeccf8a5c421bad7d07634af4
Yura <90028629+YraMorg@users.noreply.github.com>
2021-09-17 11:32:12 +0300

    Add files via upload

---
+++ Практична робота.py
@@ -0,0 +1,32 @@
+print('завдання 1,2')
+print("Hello world")
+print("my name is Yura")
+print("my age 17")
+print("I like playing dota 2")
+print()
+print()
+print('завдання 3')
+height = float(input("Please enter the height: "))
+width  = float(input("please enter the width: "))
+
+area = height * width
+
+print ("The area is {}".format(area))
+print()
+print()
+print("завдання 4")
+x = int(input("Введіть перше число "))
+y = int(input("Введіть друге число "))
+
+print("Сума ", x+y)
+print("Різниця: ", x-y)
+print("Добуток: ", x*y)
+print("Діленя ", x/y)
+print("Частка 2 чисел ", x%y)
+print()
+print()
+print("Завдання 5")
+from math import pi
+r = float(input ("Введіть радіус кола : "))
+print ("Плоша кола с радіусом : " + str(r) + " is: " + str(pi * r**2))
+
